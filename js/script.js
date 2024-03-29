3d786a696e354b9c9fd6e2259fb4b9a0c85e5e14
sofiacottone <cottonesofia@outlook.it>
2024-03-29 15:47:26 +0100

    feat: add carousel functions on click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Partendo dal markup fornito nello starter pack, rifare lo slider ma questa volta usando Vue.
+//     Bonus:
+// 1 - al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2 - applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3 - quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
+
 const slides = [
     {
         image: 'img/01.webp',
@@ -29,7 +36,62 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
-        };
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+            activeImage: 0,
+            autoSlide: null
+        }
+    },
+    methods: {
+        // show next image
+        showNextImage: function () {
+            if (this.activeImage < this.slides.length - 1) {
+                this.activeImage++;
+            } else {
+                this.activeImage = 0;
+            }
+        },
+        // show previous image 
+        showPreviousImage: function () {
+            if (this.activeImage > 0) {
+                this.activeImage--;
+            } else {
+                this.activeImage = this.slides.length - 1;
+            }
+        },
+        activateThumb: function (clickedIndex) {
+            this.activeImage = clickedIndex;
+        },
+        startAutoPlay: function () {
+            this.autoSlide = setInterval(this.showNextImage, 3000);
+        },
+        stopAutoPlay: function () {
+            clearInterval(this.autoSlide);
+            this.autoSlide = null;
+        }
+    },
+    mounted() {
+        this.startAutoPlay;
     }
 }).mount('#app');
